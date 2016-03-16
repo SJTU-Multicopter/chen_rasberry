f3634f0f80ef7d532a5023cd7ba11142d7e7f951
Dongwei <dr.dongwei@gmail.com>
2016-03-16 08:49:08 +0000

    new

--- mavros/mavros_extras/src/plugins/laser_distance.cpp
+++ mavros/mavros_extras/src/plugins/laser_distance.cpp
@@ -2,6 +2,7 @@
 #include <pluginlib/class_list_macros.h>
 #include <mavros_extras/LaserDistance.h>
 #include "std_msgs/Float32.h" 
+#include "mavros_extras/LaserDistance.h"
 #include <ros/console.h>
 
 namespace mavplugin{
@@ -19,7 +20,8 @@ public:
         
     	laser_distance_nh.param<std::string>("frame_id", frame_id, "laser_distance");
         //subcribe the topic and excute the callback function
-    	laser_distance_sub = laser_distance_nh.subscribe("/crop_dist",5,&LaserDistancePlugin::laser_distance_send_cb,this);
+    	laser_distance_sub = laser_distance_nh.subscribe("/laser_send",5,&LaserDistancePlugin::laser_distance_send_cb,this);
+    	crop_distance_sub = laser_distance_nh.subscribe("/crop_dist",5,&LaserDistancePlugin::crop_distance_send_cb,this);
 
     }
     
@@ -38,7 +40,9 @@ public:
 private:
 	ros::NodeHandle laser_distance_nh;
 	ros::Subscriber laser_distance_sub;
+	ros::Subscriber crop_distance_sub;
 	UAS *uas;
+	mavros_extras::LaserDistance value;
 
 	std::string frame_id;
 
@@ -48,14 +52,20 @@ private:
     	mavlink_msg_laser_distance_pack_chan(UAS_PACK_CHAN(uas),&laser_distance_msg,a,b,c,d); //pack
     	UAS_FCU(uas)->send_message(&laser_distance_msg); //send
         
-        ROS_INFO("float_b %d %d", laser_distance_msg.seq,laser_distance_msg.len);
+       // ROS_INFO("float_b %d %d", laser_distance_msg.seq,laser_distance_msg.len);
     	
     }
     
     //callbacks
-    void laser_distance_send_cb(const std_msgs::Float32 &msg){
-        laser_distance_send(msg.data,0.0,0.0,0.0);
+    void crop_distance_send_cb(const std_msgs::Float32 &msg){
+	value.laser_x = msg.data;    
     }
+    void laser_distance_send_cb(const mavros_extras::LaserDistance &msg){
+        value.min_distance = msg.min_distance;
+	value.angle = msg.angle;
+	laser_distance_send(value.min_distance,value.angle,value.laser_x,value.laser_y);
+    }
+
 };
 
 };
